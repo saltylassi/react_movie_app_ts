946036341d00a76dd523a18030f865e0575a624e
illke <illke@tobesoft.com>
2021-08-27 09:00:47 +0900

    electron

---
+++ public/Main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1366,
+    height: 768,
+  });
+
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, '/../build/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    });
+
+  win.loadURL(startUrl);
+}
+
+app.on('ready', createWindow);
